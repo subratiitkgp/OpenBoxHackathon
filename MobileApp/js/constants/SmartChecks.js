dd1beda30513f8c1d30ed78de79c902c6bb9ce23
Subrat Panda <subrpan@amazon.com>
2018-05-11 20:16:22 +0530

    Added Checks

---
+++ MobileApp/js/constants/SmartChecks.js
@@ -0,0 +1,33 @@
+'use strict';
+
+export const Category = {
+  APPAREL: {key:"APPAREL",value:"Apparel"},
+  MOBILE: {key:"MOBILE", value:"Mobile phone"}
+}
+
+export const CheckTypes = {
+  MULTICHOICE: {key: "MULTICHOICE", value: "Multi-Choice"},
+  BOOLEAN: {key: "BOOLEAN", value: "Boolean"},
+  BOOLEANWITHDATA: {key: "BOOLEANWITHDATA", value: "Boolean with data"},
+  BOOLEANWITHIMAGE: {key: "BOOLEANWITHIMAGE", value: "Boolean with image"}
+}
+
+export const CheckNames = {
+  CONDITION: {key: "CONDITION", value: "Is the condition of the item good ?"},
+  IMAGE: {key: "IMAGE", value: "Does the item match the image ?"},
+  SERIAL: {key: "IMAGE", value: "Does the serial number of the device match ?"},
+  ACCESSORIES : {key: "ACCESSORIES", value: "Are the following accessories present ?"},
+}
+
+export const SmartBoxChecks = {
+  MOBILE: [
+      {checkName:CheckNames.CONDITION.key, checkType:CheckTypes.BOOLEAN.key},
+      {checkName:CheckNames.IMAGE.key, checkType:CheckTypes.BOOLEANWITHIMAGE.key},
+      {checkName:CheckNames.SERIAL.key, checkType:CheckTypes.BOOLEANWITHDATA.key},
+      {checkName:CheckNames.ACCESSORIES.key, checkType:CheckTypes.MULTICHOICE.key}
+  ],
+  APPAREL: [
+      {checkName:CheckNames.CONDITION.key, checkType:CheckTypes.BOOLEAN.key},
+      {checkName:CheckNames.IMAGE.key, checkType:CheckTypes.BOOLEANWITHIMAGE.key}
+  ]
+}
